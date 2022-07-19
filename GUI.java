1ca15338a318e795250743e7c58498408842c18f
Chris Litting <mr_litting@hotmail.com>
2022-07-19 23:49:31 +0000

    updates to GUI and Input classes

--- GUI.java
+++ GUI.java
@@ -4,50 +4,60 @@
 
 public class GUI {
 	
-	private static final String SELECT_OPTION = "Please select an option";
+	private static final String SELECT_OPTION = "Please select an option: ";
+	private static final String GOODBYE_MSG = "Goodbye";
 	public static void main(String[] args) {
-
-		mainMenu();
-		//State.setDefaultSize(20);
-		//Community.setDefaultSize(10);
-		
-		//*Community.setDefaultResources(0.0);
-
-		/*
-		final int iterationsPerRound = 10000;
-		final int rounds = 1;
-		
-		State ste1 = new State();
-		*/
-		//*ste1.displayCommunitiesInfo();
-		/*
-		System.out.println();
-		for (int i = 0; i < rounds; i++){
-			ste1.iterate(iterationsPerRound);
-			ste1.displayCommunitiesInfo();
-		}
-		*/
-
-		//*ste1.communities[0].agentReport();
-		
-		
+		mainMenu();	
 	}
 
 	public static void mainMenu() {
 		boolean cont = true;
 		int value = -1;
 		while (cont) {
-			value = Menu.create(SELECT_OPTION, "do this", "do that", "quit");
-			System.out.println("Option selected: " + value);
-
-			if (value == 0) {
-				cont = false;
+			value = Menu.create(
+				SELECT_OPTION, 
+				"do this", 
+				"do that", 
+				"Quit"
+			);
+			switch (value) {
+				case 1:
+					System.out.println("doing this");
+					break;
+				case 2:
+					System.out.println("doing that");
+					break;
+				case 0:
+					System.out.println(GOODBYE_MSG);
+					cont = false;
+					break;
 			}
+
+			
 		}
 	}
 
 	public static void runSimulation() {
-
+		State.setDefaultSize(20);
+		Community.setDefaultSize(10);
+		
+		//*Community.setDefaultResources(0.0);
+		
+		final int iterationsPerRound = 10000;
+		final int rounds = 1;
+		
+		State ste1 = new State();
+		
+		//*ste1.displayCommunitiesInfo();
+		
+		System.out.println();
+		for (int i = 0; i < rounds; i++){
+			ste1.iterate(iterationsPerRound);
+			ste1.displayCommunitiesInfo();
+		}
+		
+		//*ste1.communities[0].agentReport();
+		
 	}
 
 	
--- Input.java
+++ Input.java
@@ -2,15 +2,19 @@ import java.util.Scanner;
 
 public class Input {
     
-    private static final String INTEGER_FORMAT = "Enter an integer:";
-    private static final String INTEGER_BETWEEN = "Enter an integer between %d and %d:";
+    private static final String INTEGER_FORMAT = "Enter an integer: ";
+    private static final String INTEGER_BETWEEN = "Enter an integer between %d and %d: ";
+    private static final String YES = "y";
+    private static final String NO = "n";
+    private static final String YES_OR_NO = "Please enter $s or $s: ";
+
 
     /**
      * Read an integer input by the user between two values (inclusive)
      * @param prompt as a String
      * @param min as an int
      * @param max as an int
-     * @return user input as an int
+     * @return int
      */
     public static int readIntBetween(String prompt, int min, int max) {
         System.out.print(prompt);
@@ -33,8 +37,36 @@ public class Input {
                 inputError = false;
             }
         } while (inputError);
-        in.close();
         return value;
+    }
+
+   /**
+    * Read a boolean input by the user in the form y/n
+    * @param prompt as a String
+    * @return boolean
+    */
+    public static boolean readBoolean(String prompt) {
+        System.out.print(prompt);
+        Scanner in = new Scanner(System.in);
+        boolean bool = false;
+        boolean inputError = false;
+        String input = "";
+        do {
+            input = in.nextLine();
+            if (input.toLowerCase().equals(YES)) {
+                bool = true;
+                inputError = false;
+            } else if (input.toLowerCase().equals(NO)) {
+                bool = false;
+                inputError = false;
+            } else {
+                System.out.print(
+                    String.format(YES_OR_NO, YES, NO)
+                );
+                inputError = true;
+            }
+        } while (inputError);
+        return bool;
     }
 
 
